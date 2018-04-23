a224a407e945ab34cffc75c071417de62121de7c
Rex <rexebin@gmail.com>
2018-04-23 13:10:38 +0100

    clean up.

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-import { workspace } from "vscode";
-/**
- * Config store.
- */
-export class Config {
-  static isSplit = workspace
-    .getConfiguration("openFile")
-    .get<boolean>("openSideBySide");
-}
--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,7 @@
 "use strict";
 
+import { ExtensionContext, languages } from "vscode";
 import { OpenRelativeFileDefinitionProvider } from "./open-file.provider";
-import { ExtensionContext, languages, workspace } from "vscode";
-import { Config } from "./config";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,13 +10,7 @@ export function activate(context: ExtensionContext) {
     languages.registerDefinitionProvider(
       { language: "*", scheme: "file" },
       new OpenRelativeFileDefinitionProvider()
-    ),
-
-    workspace.onDidChangeConfiguration(() => {
-      Config.isSplit = workspace
-        .getConfiguration("openFile")
-        .get("openSideBySide");
-    })
+    )
   );
 }
 
